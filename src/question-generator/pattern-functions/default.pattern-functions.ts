79c21fdc2a62027d891e6774461636b6d561a19e
Vitalii <vitalijborisenko27@gmail.com>
2026-03-06 20:44:06 +0200

    exclude generic id repeat for players

--- src/question-generator/pattern-functions/default.pattern-functions.ts
+++ src/question-generator/pattern-functions/default.pattern-functions.ts
@@ -145,7 +145,7 @@ export const GenNumberAnswers = (args: Arguments) => {
 export const GenQuestionID = (...details: string[]) => {
     try{
         const identificator = randomNumber([0,+(new Date())]) * Math.random();
-        let id = `generic:${identificator}`;
+        let id = `generic:${identificator}:${details[0] || ''}`;
     
         for(const detail of details){
             id.concat(`:${detail}`);
